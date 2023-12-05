4ccfdf784bb9809a1fb1c7029d9b0e3b76fdf9b8
CalebRdgz <calebrodgz@gmail.com>
2023-12-05 11:58:44 -0600

    doubly linked list methods done

--- 2-doubly_linked_lists.py
+++ 2-doubly_linked_lists.py
@@ -118,10 +118,65 @@ class DoublyLinkedList:
                 temp = temp.next
         #if index of node is more than half of dlinkedlist, iterate tail temp to prev nodes
         else:
+            #start temp at the tail instead of the head:
             temp = self.tail
             #range(start, stop, step (-1 = step backwards))
             for _ in range(self.length - 1, index, -1):
                 temp = temp.prev
+        #only use temp.value here to test if we got the right nodes:
+        return temp
+    
+    #set(update) a value where index is input:
+    def set_value(self, index, value):
+        #variable that stores the node using input index
+        temp = self.get(index)
+        #if temp is not None:
+        if temp:
+            #update the temp value to the input value
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+        #point the new node's previous arrow to the before node
+        new_node.prev = before
+        #point the new node's next arrow to the after node
+        new_node.next = after
+        #point before node's next arrow to the new node
+        before.next = new_node
+        #point after node's prev arrow to the new node
+        after.prev = new_node
+        #increase length by 1
+        self.length += 1
+        return True
+    
+    def remove(self, index):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        temp = self.get(index)
+        #temp.next.prev = the prev arrow of the next node after temp, point it to the node before temp:
+        temp.next.prev = temp.prev
+        #temp.prev.next = the next arrow of the prev node before temp, point it to the node after temp:
+        temp.prev.next = temp.next
+        #pop off the temp Node from the list:
+        temp.next = None
+        temp.prev = None
+        #decrement the length:
+        self.length -= 1
+        return temp.value
 
 
 # #PRINT_LIST
@@ -170,3 +225,46 @@ class DoublyLinkedList:
 # my_doubly_linked_list.print_list()
 
 #GET
+# my_doubly_linked_list = DoublyLinkedList(0)
+# my_doubly_linked_list.append(1)
+# my_doubly_linked_list.append(2)
+# my_doubly_linked_list.append(3)
+
+# print(my_doubly_linked_list.get(1))
+# print(my_doubly_linked_list.get(2))
+
+#SET
+# my_doubly_linked_list = DoublyLinkedList(11)
+# my_doubly_linked_list.append(3)
+# my_doubly_linked_list.append(23)
+# my_doubly_linked_list.append(7)
+# #print original list:
+# print("ORIGINAL LIST")
+# my_doubly_linked_list.print_list()
+
+# my_doubly_linked_list.set_value(1,4)
+# print("UPDATED LIST")
+# my_doubly_linked_list.print_list()
+
+#INSERT
+# my_doubly_linked_list = DoublyLinkedList(1)
+# my_doubly_linked_list.append(3)
+# print("ORIGINAL LIST")
+# my_doubly_linked_list.print_list()
+
+# my_doubly_linked_list.insert(1, 2)
+# print("UPDATED LIST")
+# my_doubly_linked_list.print_list()
+
+#REMOVE
+# my_doubly_linked_list = DoublyLinkedList(0)
+# my_doubly_linked_list.append(1)
+# my_doubly_linked_list.append(2)
+# print("ORIGINAL LIST:")
+# my_doubly_linked_list.print_list()
+
+# print("REMOVED NODE:")
+# print(my_doubly_linked_list.remove(1), '\n')
+
+# print("UPDATED LIST:")
+# my_doubly_linked_list.print_list()
